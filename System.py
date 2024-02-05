a5fde386833f5897b6c87c1e0dfd3a177774fd00
Lynxtail <l.gorbachev@mail.ru>
2024-02-05 18:53:23 +0400

    add commentaries

--- System.py
+++ System.py
@@ -3,31 +3,42 @@ from math import log
 from numpy import random
 import scipy.stats
 
+
 class Mx_M_C:
+
+    # конструктор
     def __init__(self, lambda_:float, servers_count:int, mu:float) -> None:
         self.lambda_ = lambda_
         self.servers_count = servers_count
         self.mu = mu
         self.servers_states = [True for _ in range(servers_count)]
         self.demands = dict() 
+        # требования описываются в виде элементов словаря:
         # {
         #   [id требования] : 
         #       [[время генерации], 
         #       [id пакета],
         #       [состояние = -1 - очередь, 0...servers_count-1 - номер прибора, servers_count - ожидает сборки]]
         # }
-        self.last_state = 0
-        self.states = {'0': 0}
-        self.packs = 0
+        self.last_state = 0 # момент времени, когда система закончила прибывать в предыдущем состоянии
+        self.states = {'0': 0} # суммарные длительности пребывания системы в соответствующих состояниях
+        self.packs = 0 # число пакетов в системе -- её состояние
         self.export_demands()
         self.export_states()
-    
+
+
+    # функции генерации случайных величин
     # ------------------------------------------------------------------
     # случайные величины генерируются методом обратного преобразования
+    
+    # промежуток времени между поступлением двух требований:
+    # экспоненциальное распределение с параметром лямбда
     def arrival_time(self) -> float:
         random.seed()
         return -log(random.random()) / self.lambda_
 
+    # размер поступившего пакета:
+    # геометрическое распределение, b - средний размер пакета
     def pack_size(self, b:float) -> int:
         random.seed()
         y = random.random()
@@ -42,6 +53,8 @@ class Mx_M_C:
             k += 1
         return k
     
+    # время обслуживания требования:
+    # экспоненциальное распределение с параметром мю
     def service_time(self) -> float:
         random.seed()
         return -log(random.random()) / self.mu
@@ -71,25 +84,31 @@ class Mx_M_C:
     #     return scipy.stats.geom(scale=1-(1/b)).rvs()
     # ------------------------------------------------------------------
 
+    # запись данных о требованиях в файл
     def export_demands(self) -> None:
         with open(f'demands.json', 'w') as f:
             json.dump(self.demands, f)
 
+    # запись данных о состояниях в файл
     def export_states(self) -> None:
         with open(f'states.json', 'w') as f:
             json.dump(self.states, f)
     
+    # получение данных о требованиях из файла
     def import_demands(self) -> dict:
         with open(f'demands.json') as f:
             return json.load(f)
 
+    # получение данных о состояних из файла
     def import_states(self) -> dict:
         with open(f'states.json') as f:
             return json.load(f)
 
+    # перечисление текущих требований в системе
     def current_demands(self) -> tuple:
         return (item for item in self.demands.keys())
     
+    # обновление данных о состояниях
     def update_time_states(self, t_now:float) -> None:
         self.states = self.import_states()
 
--- main.py
+++ main.py
@@ -1,49 +1,59 @@
 import numpy as np
 from System import Mx_M_C
 
-
+# основная функция, осуществляющая моделирование
 def simulation(system:Mx_M_C, b:float):
     t = 0 # текущее модельное время
     t_max = round(10**6 / lambda_) # максимальное модельное время
-    pack = 0
-    schedule = [t_max + 1] * (system.servers_count + 1)
-    schedule[0] = 0
-    ready_packs_count = 0
-    sum_packs_life_time = 0
+    # t_max = 10**2
+    pack = 0 # номер очередного пакета
+    schedule = [t_max + 1] * (system.servers_count + 1) # таблица расписания событий
+    schedule[0] = 0 # генерация очередного требования произойдёт в момент времени 0
+    ready_packs_count = 0 # число обслуженных пакетов
+    sum_packs_life_time = 0 # суммарная длительность пребывания всех обслуженных пакетов в системе
 
     while t < t_max: # происходит процесс имитации
-        indicator = False
+        indicator = False # указывает на то, происходит сейчас какое-то событие или 
+                          # нужно продвинуть модельное время
         print(f'{t}:')
         # system.import_demands()
         
+        # генерация пакета
         if schedule[0] == t:
             indicator = True
-            schedule[0] = t + system.arrival_time()
-            pack += 1
-            demands_count = system.pack_size(b)
+            schedule[0] = t + system.arrival_time() # определение времени следующей генерации
+            pack += 1 
+            demands_count = system.pack_size(b) # определение размера пакета
             system.packs += 1
             print(f'\tПакет {pack} из {demands_count} требований поступил в систему')
+            # создание требований из пакета
             for i in range(1, demands_count + 1):
                 system.demands[pack + i * 10 ** -(demands_count // 10 + 1)] = [t, pack, -1]
             system.update_time_states(t)
         
+        # начало обслуживание очередного требования из очереди
         if any(system.servers_states) and any([demand[-1] == -1 for demand in system.demands.values()]):
             indicator = True
+            # определение номера свободного прибора
             server = system.servers_states.index(True)
             system.servers_states[server] = False
+            # "привязка" взятого требования к этому прибору
             for demand in system.demands.keys():
                 if system.demands[demand][-1] == -1:
                     system.demands[demand][-1] = server
                     break
+            # определение времени завершения обслуживания
             schedule[server + 1] = t + system.service_time()
             print(f'\tтребование {demand} начало обслуживаться на приборе {server + 1}')
 
+        # завершение обслуживания требования
         if any([process == t for process in schedule[1:]]):
             indicator = True
             server = schedule.index(t) - 1
             if server == -1:
                 raise Exception
             system.servers_states[server] = True
+            # переход требования в состояние "готово к сборке"
             for serviced_demand in system.demands.keys():
                 if system.demands[serviced_demand][-1] == server:
                     system.demands[serviced_demand][-1] = system.servers_count
@@ -52,15 +62,22 @@ def simulation(system:Mx_M_C, b:float):
             print(f'\tтребование {serviced_demand} завершило обслуживаться на приборе {server + 1} и ожидает сборки')
 
             # проверка: если все требования одного пакета ожидают сборки, то требование выходит из системы
+
+            # сборка всех требований одного пакета в список
             new_pack = list()
             for other_demand in system.demands.keys():
                 if system.demands[other_demand][1] == system.demands[serviced_demand][1]:
                     new_pack.append((other_demand, system.demands[other_demand]))
+            
+            # проверка, все ли требования этого списка обслужились
             flag = True
             for item in new_pack:
                 if item[1][-1] != system.servers_count:
                     flag = False
                     break
+            
+            # при успешной проверке, пакет покидает систему,
+            # а его требования удаляются
             if flag:
                 print('\tтребования:', end=' ')
                 for item in new_pack:
@@ -70,8 +87,10 @@ def simulation(system:Mx_M_C, b:float):
                 system.packs -= 1
                 ready_packs_count += 1
                 sum_packs_life_time += t - item[1][0]
+            
             system.update_time_states(t)
 
+        # если ни одно событие не произошло, то переход к новому моменту времени
         if not indicator:
             system.update_time_states(t)
             # print(schedule)
@@ -89,9 +108,9 @@ def simulation(system:Mx_M_C, b:float):
     print(f'Проверка оценки стационарного распредления: {sum([state / t_max for state in system.import_states().values()])}')
 
 if __name__ == "__main__":
-    lambda_ = 1 / 10
-    servers_count = 150
-    mu = 1 / 281
-    b = 5
+    lambda_ = 1 / 10 # интенсивность входящего потока
+    servers_count = 150 # число приборов
+    mu = 1 / 281 # интенсивность обслуживания
+    b = 5 # средний размер пакета
     system = Mx_M_C(lambda_, servers_count, mu)
     simulation(system, b)
